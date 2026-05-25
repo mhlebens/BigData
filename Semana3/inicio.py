f5da46a7f9e9cfd38be593dfbb750d89c372ad9a
mhlebens <marifermatah@gmail.com>
2026-05-25 10:25:32 -0600

    first commit

---
+++ Semana3/inicio.py
@@ -0,0 +1,25 @@
+print("Saludos clase") # mostrar mensajes en consola
+
+# "()" pasar parámetros
+
+textNombre = "mafeh" #Tipo de dato: texto
+edad = 20 #Tipo de dato: numero entero
+numeros = 0.13 #Tipo de dato: numero decimal
+valores = True #Tipo de dato: boolean
+
+print(textNombre)
+print(edad)
+print(numeros)
+print(valores)
+
+#todo lo que está antes de unos parentesís se les conoce como funciones
+soyUnaVariable = input("Ingrese su nombre: ")
+# "," sirve para pasar varios parámetros
+print("Saludo usuario: ", soyUnaVariable)
+
+a = int(input("Ingrese su numero a: "))
+b = int(input("Ingrese su numero b: "))
+
+resultadoSuma = a + b
+
+print("El resultado de la suma es:", resultadoSuma)
