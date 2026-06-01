25c68d2ed909f5d6419e520a163dc0d591d9b94a
mhlebens <marifermatah@gmail.com>
2026-06-01 08:55:33 -0600

    Inicio semana 4

---
+++ Semana4/funciones.py
@@ -0,0 +1,19 @@
+#def = function, sirve para funciones
+#La identación se genera con la tecla Tab
+
+def saludo():
+    print("Bienvenido al sistema Bigdata 2026")
+
+def saludoPersonalizado(recibimosParametro):
+    print("Bienvenido al sistema Bigdata 2026 ", recibimosParametro)
+
+def saludoPersonalizado2(nombre, apellido):
+    print("Bienvenido al sistema Bigdata 2026 ", nombre, apellido)
+
+resultadoSuma = 1+3
+
+print(resultadoSuma)
+
+saludo()
+saludoPersonalizado("Mafeh")
+saludoPersonalizado2 ("Mafeh","Mata")
